c6cb137673332135b1f058978db30e6e8b2047ca
charlyoleg <charlyoleg@fabfolk.com>
2021-07-06 16:34:38 +0200

    fix npm run

--- core/ts/srv/nty_web_server.ts
+++ core/ts/srv/nty_web_server.ts
@@ -8,5 +8,5 @@ const PORT = 8089;
 app.get('/', (req, res) => res.send('nty_web_server says hello'));
 
 app.listen(PORT, () => {
-  console.log(`[nty_web_server]: Server is running at https://localhost:${PORT}`);
+  console.log(`[nty_web_server]: Server is running at http://localhost:${PORT}`);
 });
