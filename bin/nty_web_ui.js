ae8410abc2759050aa9d2fcca9e70cceb32b8b55
charli <charlyoleg@fabfolk.com>
2021-07-08 23:12:48 +0200

    pretty the code

--- bin/nty_web_ui.js
+++ bin/nty_web_ui.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 // nty_web_ui.js
-console.log("Starting Nty-web-ui, the local web server of Nty, ...");
-// using express-js
-//require('../core/dist/srv/nty_web_server.js'); // commonjs module
-import {make_express_app, express_listen_callback, NTY_PORT} from '../core/dist/srv/nty_web_server.js'; // es6 (aka es2015) module
 
+console.log("Starting Nty-web-ui, the local web server of Nty, ...");
+
+// the way 'commonjs module'
+//require('../core/dist/srv/nty_web_server.js');
+
+// the way 'es6 (aka es2015) module'
+import {make_express_app, express_listen_callback, NTY_PORT} from '../core/dist/srv/nty_web_server.js';
 const app = make_express_app();
 app.listen(NTY_PORT, express_listen_callback);
 
+console.log("end of nty_web_ui.js");
