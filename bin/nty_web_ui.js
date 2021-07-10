7a8882bdbd7075308e552ee0983096be5f09c95f
charli <charlyoleg@fabfolk.com>
2021-07-10 16:16:47 +0200

    micro refactoring 2

--- bin/nty_web_ui.js
+++ bin/nty_web_ui.js
@@ -7,8 +7,7 @@ console.log("Starting Nty-web-ui, the local web server of Nty, ...");
 //require('../core/dist/srv/nty_web_server.js');
 
 // the way 'es6 (aka es2015) module'
-import {generate_nty_web_server, listen_callback_function, NTY_PORT} from '../core/dist/srv/nty_web_server.js';
-const nty_app = generate_nty_web_server();
-nty_app.listen(NTY_PORT, listen_callback_function);
+import {nty_web_server_listen} from '../core/dist/srv/nty_web_server.js';
+nty_web_server_listen();
 
 console.log("end of nty_web_ui.js");
--- core/ts/srv/nty_web_server.ts
+++ core/ts/srv/nty_web_server.ts
@@ -9,20 +9,11 @@ const app = express();
 app.get('/', (req, res) => res.send('nty_web_server says hello'));
 
 
-function generate_nty_web_server () {
-  return app;
+function nty_web_server_listen () {
+  app.listen(NTY_PORT, () => {
+    console.log(`[nty_web_server]: Server is running at http://localhost:${NTY_PORT}`);
+  });
 }
 
-function listen_callback_function () {
-  console.log(`[nty_web_server]: Server is running at http://localhost:${NTY_PORT}`);
-}
-
-//app.listen(NTY_PORT, () => {
-//  console.log(`[nty_web_server]: Server is running at http://localhost:${NTY_PORT}`);
-//});
-
-//const nty_app = generate_nty_web_server();
-//nty_app.listen(NTY_PORT, listen_callback_function);
-
-export {generate_nty_web_server, listen_callback_function, NTY_PORT};
+export {nty_web_server_listen};
 
